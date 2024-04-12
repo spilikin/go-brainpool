d0df75b07e7c517a58ffb306ad5ccf0cf9a9a535
spilikin <108782+spilikin@users.noreply.github.com>
2024-04-12 08:06:53 +0200

    First commit

---
+++ brainpool_test.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"testing"
+
+	"github.com/spilikin/go-brainpoll/x509brainpool"
+)
+
+// sample keys and certificates from https://github.com/gematik/erp-e2e-testsuite
+var testKeyBytes = []byte(`-----BEGIN EC PRIVATE KEY-----
+MHgCAQEEID0gGM/y+gETNO9kFWY6vELk9rjq2H/tuZogskG/rpB8oAsGCSskAwMC
+CAEBB6FEA0IABGc0iasne1f/tcuTSw4CiRfxZ3LvLsQDuMmbqTO5CuOsXND+n8fV
+Q3zyzOv7imMbR7nqdiEFGAHtGNznbsLrU80=
+-----END EC PRIVATE KEY-----`)
+
+var testCertBytes = []byte(`-----BEGIN CERTIFICATE-----
+MIIDeDCCAx6gAwIBAgIHArLLcBI3KDAKBggqhkjOPQQDAjCBmjELMAkGA1UEBhMC
+REUxHzAdBgNVBAoMFmdlbWF0aWsgR21iSCBOT1QtVkFMSUQxSDBGBgNVBAsMP0lu
+c3RpdHV0aW9uIGRlcyBHZXN1bmRoZWl0c3dlc2Vucy1DQSBkZXIgVGVsZW1hdGlr
+aW5mcmFzdHJ1a3R1cjEgMB4GA1UEAwwXR0VNLlNNQ0ItQ0E1MSBURVNULU9OTFkw
+HhcNMjMxMTA5MjMwMDAwWhcNMjgxMTA5MjI1OTU5WjCBhDELMAkGA1UEBhMCREUx
+HDAaBgNVBAoMEzEwMjMxMDgwMSBOT1QtVkFMSUQxFTATBgNVBAQMDFJvc2Vuc3Ry
+YXVjaDEOMAwGA1UEKgwFQmVybmQxMDAuBgNVBAMMJ0FyenRwcmF4aXMgQmVybmQg
+Um9zZW5zdHJhdWNoIFRFU1QtT05MWTBaMBQGByqGSM49AgEGCSskAwMCCAEBBwNC
+AARnNImrJ3tX/7XLk0sOAokX8Wdy7y7EA7jJm6kzuQrjrFzQ/p/H1UN88szr+4pj
+G0e56nYhBRgB7Rjc527C61PNo4IBYDCCAVwwLAYDVR0fBCUwIzAhoB+gHYYbaHR0
+cDovL2VoY2EuZ2VtYXRpay5kZS9jcmwvMA4GA1UdDwEB/wQEAwIHgDATBgNVHSUE
+DDAKBggrBgEFBQcDAjAdBgNVHQ4EFgQUshKrzlr5DNSiNzsEXWLuScZAN2wwDAYD
+VR0TAQH/BAIwADAfBgNVHSMEGDAWgBQGmOkCVf/Jn1yjZQ7xXeIg9YT7kzA7Bggr
+BgEFBQcBAQQvMC0wKwYIKwYBBQUHMAGGH2h0dHA6Ly9laGNhLmdlbWF0aWsuZGUv
+ZWNjLW9jc3AwWgYFKyQIAwMEUTBPME0wSzBJMEcwFgwUQmV0cmllYnNzdMOkdHRl
+IEFyenQwCQYHKoIUAEwEMhMiMS0yLUFSWlRQUkFYSVMtQmVybmRSb3NlbnN0cmF1
+Y2gwMTAgBgNVHSAEGTAXMAoGCCqCFABMBIEjMAkGByqCFABMBE0wCgYIKoZIzj0E
+AwIDSAAwRQIgWlSdCIw6Z6alM+dGnA4vfkxDoViIqJMw/PH4U0VUmNsCIQCcX9UW
+JnSDBKGp4nZTcuozRPsJK47cBkil0x6Zrkoxkg==
+-----END CERTIFICATE-----`)
+
+func TestBrainpool(t *testing.T) {
+	priv, err := ecdsa.GenerateKey(x509brainpool.P256r1(), rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(priv)
+
+	pemBlock, _ := pem.Decode(testKeyBytes)
+	if pemBlock == nil {
+		t.Fatal("failed to decode PEM block containing private key")
+	}
+
+	priv2, err := x509brainpool.ParseECPrivateKey(pemBlock.Bytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pemBlock, _ = pem.Decode(testCertBytes)
+
+	cert, err := ParseCertificate(pemBlock.Bytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(cert)
+
+	t.Log(priv2)
+}
+
+func TestSignature(t *testing.T) {
+	priv, err := parsePEMKey(testKeyBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msg := []byte("test")
+	r, s, err := ecdsa.Sign(rand.Reader, priv, msg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cert, err := parsePEMCert(testCertBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ecdsaPubKey, ok := cert.PublicKey.(*ecdsa.PublicKey)
+	if !ok {
+		t.Fatal("failed to convert public key to ECDSA public key")
+	}
+
+	if !ecdsa.Verify(ecdsaPubKey, msg, r, s) {
+		t.Fatal("signature didn't verify.")
+	}
+}
+
+func parsePEMKey(pemBytes []byte) (*ecdsa.PrivateKey, error) {
+	pemBlock, _ := pem.Decode(pemBytes)
+	if pemBlock == nil {
+		return nil, nil
+	}
+
+	return x509brainpool.ParseECPrivateKey(pemBlock.Bytes)
+}
+
+func parsePEMCert(pemBytes []byte) (*x509.Certificate, error) {
+	pemBlock, _ := pem.Decode(pemBytes)
+	if pemBlock == nil {
+		return nil, nil
+	}
+
+	return ParseCertificate(pemBlock.Bytes)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/spilikin/go-brainpoll
+
+go 1.22.2
+
+require golang.org/x/crypto v0.22.0
